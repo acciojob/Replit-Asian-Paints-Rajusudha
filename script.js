7392b282c2ed60a5e8e2ea9c12ba6eb6834e6633
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-01-26 09:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const gridcontainer=document.querySelector(".grid");
+const form=document.querySelector("#form");
+let previousSelectedElement=null;
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let cellid=form.inputId.value;
+    let selectedcolor=form.color.value;
+    let cell=document.getElementById(cellId);
+    cell.style.backgroundColor=selectedcolor;
+    if(previousSelectedElement){
+        previousSelectedElement.style.backgroundColor="white";
+    }
+    previousSelectedElement=cell;
+});
+for(let i=1;i<=9;i++){
+    const gridItem=document.createElement("div");
+    gridItem.id=i;
+    gridItem.innerText=i;
+    gridItem.className="grid-item";
+    gridcontainer.appendChild(gridItem);
+}
